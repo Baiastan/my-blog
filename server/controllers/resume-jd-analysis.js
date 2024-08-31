aaad41635c98dcba36fac713c062146a97493058
Baiastan Zhuzupbekov <baiastan@Baiastans-MacBook-Pro.local>
2024-08-31 13:03:38 -0700

    server folder deleted

--- server/controllers/resume-jd-analysis.js
+++
@@ -1,7 +0,0 @@
-export const getResumeJdAnalysis = async (req, res) => {
-  const { data } = req.body;
-
-  console.log(data);
-
-  res.json({ message: "Success" });
-};
--- server/routes/resume-jd-analysis.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-import { getResumeJdAnalysis } from "../controllers/resume-jd-analysis.js";
-
-const router = express.Router();
-
-router.post("/system/resume-jd-analysis", getResumeJdAnalysis);
-
-export default router;
